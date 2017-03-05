17c9cd14fc0b227ec06ada91bc34ccc9759fdb19
Kanter666 <ak2g15@soton.ac.uk>
2017-03-05 01:03:05 +0000

    Added example of passing data to file
    Showing which formats are functional, TODO: update NameIDMap , most of the values are not available to download, use values from website

--- src/Main.py
+++ src/Main.py
@@ -5,39 +5,64 @@ from DownloadData import Downloader
 class loader():
 	
 	def __init__(self):
+		self.parentDir = os.path.abspath('..')
 		self.time=""
 		self.date=""
 		self.param=""
 		self.target=""
+		self.IDdict = dict()
+		self.loadNameIDMap('NameIDMap')
 
 	def testData(self):
 		self.time="00"
 		self.date="2013-09-01/to/2013-09-30"
 		self.param="165.128/41.128"
-		self.target="test1.grib"
+		self.target=self.parentDir+'/files/test1.grib'
+
+	def testEntry1(self):
+		self.loadData("00/12", "2013-08-01/to/2013-10-01", ["10mUwind","10mVwind"])
+		self.setFileName("testEntry1")
+		print(self.time)
+		print(self.date)
+		print(self.param)
+		print(self.target)
+		self.downloadData()
+
+
+	def loadData(self, time, date, param):
+		self.time = time
+		self.date = date
+		for x in param:
+			print(x)
+			idNumber = self.IDdict[x].rstrip()	#.rstrip() is for removing newline character that is appended at the end of string
+			self.param+=idNumber+".128/"
+		self.param = self.param[:-1]
+
+	def setFileName(self, name):
+		self.target=self.parentDir+'/files/'+name+".grib"
+
 
 	def downloadData(self):
 		down = Downloader()
 		down.downloadData(self.time, self.date, self.param, self.target)
 
-#function that loads dictionary of types of data and it's IDs 
-#example param: 'NameIDMap'
-def loadNameIDMap(fileName):
-	parentPath = os.path.abspath('..')
-	s = open(parentPath+'/files/'+fileName, 'r')
-	IDdict = dict()
-	for line in s:
-		print(line)
-		splits = line.split(' ')
-		print(splits)
-		IDdict[splits[0]] = splits[1]
-	for x in IDdict:
-		print(x + " " + IDdict[x])
+	#function that loads dictionary of types of data and it's IDs 
+	#example param: 'NameIDMap'
+	def loadNameIDMap(self, fileName):
+		s = open(self.parentDir+'/files/'+fileName, 'r')
+		for line in s:
+			print(line)
+			splits = line.split(' ')
+			print(splits)
+			self.IDdict[splits[0]] = splits[1]
+		for x in self.IDdict:
+			print(x + " " + self.IDdict[x])
+
+
 
 
 
 
 if __name__ == "__main__":
 	load = loader()
-	load.testData()
-	load.downloadData()
+	load.testEntry1()
